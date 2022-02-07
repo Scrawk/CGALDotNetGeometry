f6a132368ae80c61d93fe3b37b42fcf898caecce
Justin <29545213+Scrawk@users.noreply.github.com>
2022-02-07 15:20:43 +0800

    added extensions

---
+++ CGALDotNetGeometry/Extensions/PrimativeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CGALDotNetGeometry.Numerics;
+
+namespace CGALDotNetGeometry.Extensions
+{
+    public static class PrimativeExtensions
+    {
+        public static bool IsNullIndex(this int i)
+        {
+            return i == MathUtil.NULL_INDEX;
+        }
+    }
+}
